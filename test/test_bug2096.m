cd52756fdd5cd182e4dc2a5378c2efa806408eaa
Robert Oostenveld <r.oostenveld@donders.ru.nl>
2013-06-20 12:36:09 +0200

    enhancement - use explicit source structures to test cifti exporting, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2096

--- test/test_bug2096.m
+++ test/test_bug2096.m
@@ -3,11 +3,38 @@ function test_bug2096
 % TEST test_bug2096
 % TEST ft_sourcewrite
 
-load(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2096/CP10168_4DEXP_3-Restin_BNN_V1_MEG_icaimagcoh_freq3.mat'));
+
+
+
+source = [];
+source.dim = [5 6 7];
+[X Y Z] = ndgrid(1:source.dim(1), 1:source.dim(2), 1:source.dim(3));
+source.transform = eye(4);
+source.pos = warp_apply(source.transform, [X(:) Y(:) Z(:)]);
+source.conn = randn(prod(source.dim));
+source.conndimord = 'pos_pos';
 
 cfg = [];
-cfg.filename = [tempname '.cii'];
+cfg.filetype = 'cifti';
+cfg.parameter = 'conn';
+cfg.filename = 'test_bug2096';
 ft_sourcewrite(cfg, source);
+
+source = [];
+source.dim = [5 6 7];
+[X Y Z] = ndgrid(1:source.dim(1), 1:source.dim(2), 1:source.dim(3));
+source.transform = eye(4);
+source.pos = warp_apply(source.transform, [X(:) Y(:) Z(:)]);
+source.time = 1:10;
+source.timeseries = randn(prod(source.dim), length(source.time));
+source.timeseriesdimord = 'pos_time';
+
+cfg = [];
+cfg.filetype = 'cifti';
+cfg.parameter = 'timeseries';
+cfg.filename = 'test_bug2096';
+ft_sourcewrite(cfg, source);
+
 
 % test reading these files
 % http://brainvis.wustl.edu/cifti/DenseConnectome.dconn.nii
@@ -16,13 +43,14 @@ ft_sourcewrite(cfg, source);
 
 p = '/home/common/matlab/fieldtrip/data/test/bug2096';
 p = '/Volumes/Data/roboos/AeroFS/bug2096';
+p = '/Users/robert/AeroFS/bug2096';
 % cii1 = ft_read_mri(fullfile(p, 'DenseConnectome.dconn.nii',          'fileformat', 'cifti');
 % cii2 = ft_read_mri(fullfile(p, 'DenseTimeSeries.dtseries.nii',       'fileformat', 'cifti');
 % cii3 = ft_read_mri(fullfile(p, 'ParcellatedTimeSeries.ptseries.nii', 'fileformat', 'cifti');
-cii1 = read_cifti(fullfile(p, 'DenseConnectome.dconn.nii'));
-cii2 = read_cifti(fullfile(p, 'DenseTimeSeries.dtseries.nii'));
-cii3 = read_cifti(fullfile(p, 'ParcellatedTimeSeries.ptseries.nii'));
-cii4 = read_cifti(fullfile(p, 'BOLD_REST2_LR.dtseries.nii'));
-cii5 = read_cifti(fullfile(p, 'BOLD_REST2_LR_Atlas.dtseries.nii'));
+cii1 = ft_sourceread(fullfile(p, 'DenseConnectome.dconn.nii'));
+cii2 = ft_sourceread(fullfile(p, 'DenseTimeSeries.dtseries.nii'));
+cii3 = ft_sourceread(fullfile(p, 'ParcellatedTimeSeries.ptseries.nii'));
+cii4 = ft_sourceread(fullfile(p, 'BOLD_REST2_LR.dtseries.nii'));
+cii5 = ft_sourceread(fullfile(p, 'BOLD_REST2_LR_Atlas.dtseries.nii'));
 
 
