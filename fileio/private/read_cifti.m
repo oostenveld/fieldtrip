7c290009655a3f1ae18fd000c7ce781bc85c006d
Robert Oostenveld <r.oostenveld@donders.ru.nl>
2013-06-20 12:32:04 +0200

    bugfix - fixed some problems related to endianness using the documentation at http://nifti.nimh.nih.gov/nifti-1/documentation/nifti1fields. See also http://bugzilla.fcdonders.nl/show_bug.cgi?id=2096

--- fileio/private/read_cifti.m
+++ fileio/private/read_cifti.m
@@ -1,10 +1,10 @@
 function cii = read_cifti(filename)
 
 % READ_CIFTI reads geometry and functional data or connectivity from a cifti file
-% 
+%
 % Use as
 %   cii = read_cifti(filename)
-% 
+%
 % See also WRITE_CIFTI
 
 % 540 bytes with nifti-2 header
@@ -18,32 +18,45 @@ function cii = read_cifti(filename)
 % read the header section
 hdr = read_nifti2_hdr(filename);
 
-vox_offset = hdr.vox_offset;
-xml_offset = 540+12;
-xml_size   = vox_offset-xml_offset-8;
+% xml_offset = 540+12;
+% xml_size   = hdr.vox_offset-xml_offset-8;
 
 fid = fopen(filename, 'rb', hdr.endian);
 
+% determine the file size, this is used to catch endian errors
+fseek(fid, 0, 'eof');
+filesize = ftell(fid);
+fseek(fid, 0, 'bof');
+
 fseek(fid, 540, 'bof');
-hdrext   = fread(fid, [1 4], 'int8');
-if ~isequal(hdrext, [1 0 0 0])
+hdrext = fread(fid, [1 4], 'int8');
+if hdrext(1)~=1
   error('cifti requires a header extension');
 end
 
-% this is an alternative to determine the size
-prexml   = fread(fid, [1 8], 'int8=>int8');
-if hdr.endian=='l'
-  xml_size = typecast(fliplr(prexml(1:4)), 'uint32') - 8;
-else
-  xml_size = typecast(prexml(1:4), 'uint32') - 8;
+% determine the size of the header extension
+esize = fread(fid, 1, 'int32=>int32');
+etype = fread(fid, 1, 'int32=>int32');
+
+hdrsize = 540+4;
+voxsize = filesize-hdr.vox_offset;
+if esize>(filesize-hdrsize-voxsize)
+  warning('the endianness of the header extension is inconsistent with the nifti-2 header');
+  esize = swapbytes(esize);
+  etype = swapbytes(etype);
 end
 
-fseek(fid, xml_offset, 'bof');
-xmldata = fread(fid, [1 xml_size], 'uint8=>char');
+if etype~=32
+  error('the header extension type is not cifti');
+end
 
+% read the extension content, subtract the 8 bytes from esize and etype
+xmldata = fread(fid, [1 esize-8], 'uint8=>char');
+
+% the size of the extension must be an integer multiple of 16 bytes according to http://nifti.nimh.nih.gov/nifti-1/documentation/nifti1fields/nifti1fields_pages/extension.html
+% consequently there might be some zero-padding at the end of the XML section
 if any(xmldata==0)
-  warning('removing some junk from xml section');
-  xmldata  = xmldata(xmldata>0);
+  xmldata = xmldata(xmldata>0);
 end
 
 % write the xml section to a temporary file
@@ -57,23 +70,24 @@ end
 % it is also included with the gifti toolbox
 xml = xmltree(xmldata);
 
-
-% read the voxel data section
-fseek(fid, vox_offset, 'bof');
-switch hdr.datatype
-  case   2, [voxdata nitemsread] = fread(fid, inf, 'uchar');
-  case   4, [voxdata nitemsread] = fread(fid, inf, 'short');
-  case   8, [voxdata nitemsread] = fread(fid, inf, 'int');
-  case  16, [voxdata nitemsread] = fread(fid, inf, 'float');
-  case  64, [voxdata nitemsread] = fread(fid, inf, 'double');
-  case 512, [voxdata nitemsread] = fread(fid, inf, 'ushort');
-  case 768, [voxdata nitemsread] = fread(fid, inf, 'uint');
-  otherwise, error('unsupported datatype');
+if false
+  % read the voxel data section
+  fseek(fid, hdr.vox_offset, 'bof');
+  switch hdr.datatype
+    case   2, [voxdata nitemsread] = fread(fid, inf, 'uchar');
+    case   4, [voxdata nitemsread] = fread(fid, inf, 'short');
+    case   8, [voxdata nitemsread] = fread(fid, inf, 'int');
+    case  16, [voxdata nitemsread] = fread(fid, inf, 'float');
+    case  64, [voxdata nitemsread] = fread(fid, inf, 'double');
+    case 512, [voxdata nitemsread] = fread(fid, inf, 'ushort');
+    case 768, [voxdata nitemsread] = fread(fid, inf, 'uint');
+    otherwise, error('unsupported datatype');
+  end
 end
 
 fclose(fid);
 
 cii.hdr = hdr;
 cii.xml = xml;
-cii.voxdata = squeeze(reshape(voxdata, hdr.dim(2:end)));
+% cii.voxdata = squeeze(reshape(voxdata, hdr.dim(2:end)));
 cii.xmldata = xmldata;
