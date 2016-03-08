f9714c7a57098be4127dc4e2cad7f390a571a020
Robert Oostenveld <r.oostenveld@gmail.com>
2016-03-08 18:01:46 +0100

    ENH - cleanups to test script

--- test/test_bug3089.m
+++ test/test_bug3089.m
@@ -4,9 +4,11 @@ function test_bug3089
 % WALLTIME 00:20:00
 
 % TEST test_bug3089
-% TEST ft_dipolefitting ft_compute_leadfield 
+% TEST ft_dipolefitting ft_compute_leadfield
 
 dataset = 'oddball1_mc_downsampled.fif';
+
+%%
 
 cfg = [];
 cfg.dataset = dataset;
@@ -20,62 +22,58 @@ cfg = ft_definetrial(cfg);
 
 %%
 
-cfg.continuous    = 'yes';
-cfg.hpfilter      = 'no';
-cfg.detrend       = 'no';
-cfg.demean        = 'yes';
+cfg.continuous     = 'yes';
+cfg.hpfilter       = 'no';
+cfg.detrend        = 'no';
+cfg.demean         = 'yes';
 cfg.baselinewindow = [-inf 0];
-cfg.dftfilter     = 'yes';
-cfg.dftfreq       = [50 100];
-cfg.lpfilter      = 'yes';
-cfg.lpfreq        = 120;
-cfg.channel       = {'MEG', 'EEG'};
-cfg.precision     = 'single';
-cfg.coilaccuracy  = 1;
+cfg.dftfilter      = 'yes';
+cfg.dftfreq        = [50 100];
+cfg.lpfilter       = 'yes';
+cfg.lpfreq         = 120;
+cfg.channel        = {'MEG', 'EEG'};
+cfg.precision      = 'single';
+cfg.coilaccuracy   = 1; % use the COILDEF file
 
- 
-data_meg = ft_preprocessing(cfg);
+data_raw = ft_preprocessing(cfg);
 
- 
-%%
+%% reject noisy trials
 
 cfg = [];
 cfg.method = 'summary';
 cfg.keepchannel = 'yes';
 
- 
 cfg.channel = 'EEG';
-data_meg_clean1 = ft_rejectvisual(cfg, data_meg);
+data_clean = ft_rejectvisual(cfg, data_raw);
 
- 
 cfg.channel = 'MEG*1';
-data_meg_clean2 = ft_rejectvisual(cfg, data_meg_clean1);
+data_clean = ft_rejectvisual(cfg, data_clean);
 
- 
 cfg.channel = {'MEG*2', 'MEG*3'};
-data_meg_clean3 = ft_rejectvisual(cfg, data_meg_clean2);
+data_clean = ft_rejectvisual(cfg, data_clean);
 
- 
-save data_meg_clean3 data_meg_clean3
+save data_clean data_clean
+
 %% reference eeg data
+
 cfg = [];
 cfg.reref = 'yes';
 cfg.refchannel = 'all';
 cfg.channel = 'EEG';
 
-data_eeg = ft_preprocessing(cfg, data_meg_clean3);
+data_eeg = ft_preprocessing(cfg, data_clean);
 
 cfg = [];
 cfg.channel = 'MEG';
 
-data_meg = ft_preprocessing(cfg, data_meg_clean3);
+data_meg = ft_preprocessing(cfg, data_clean);
 
-data = ft_appenddata([], data_eeg, data_meg);
+data_all = ft_appenddata([], data_eeg, data_meg);
 
-%%
+%% average
 
 cfg = [];
-timelock_all = ft_timelockanalysis(cfg, data);
+timelock_all = ft_timelockanalysis(cfg, data_all);
 
 save timelock_all timelock_all
 
@@ -85,22 +83,20 @@ cfg = [];
 cfg.layout = 'neuromag306mag.lay';
 figure; ft_multiplotER(cfg, timelock_all);
 
- 
 cfg.layout = 'neuromag306planar.lay';
 figure; ft_multiplotER(cfg, timelock_all);
-
 
 cfg = [];
 cfg.elec = timelock_all.elec;
 cfg.layout = ft_prepare_layout(cfg);
 figure; ft_multiplotER(cfg, timelock_all);
- 
+
 %%
 
 cfg = [];
 cfg.covariance = 'yes';
 cfg.covariancewindow = [-0.2 0];
-timelock_cov = ft_timelockanalysis(cfg, data);
+timelock_cov = ft_timelockanalysis(cfg, data_all);
 
 save timelock_cov timelock_cov
 
@@ -120,75 +116,87 @@ timelock_all.elec  = ft_convert_units(timelock_all.elec , 'm');
 timelock_cov.grad  = ft_convert_units(timelock_cov.grad , 'm');
 timelock_cov.elec  = ft_convert_units(timelock_cov.elec , 'm');
 
-%% 
+%% recompute the EEG headmodel
+
 cfg = [];
 cfg.method = 'bemcp';
 headmodel_eeg = ft_prepare_headmodel(cfg, headmodel_eeg);
 
+%% conventional dipole fitting
 
-%% dipole fitting 
 cfg = [];
-cfg.latency = 0.100;
-cfg.numdipoles = 2;
-cfg.symmetry = 'x';
-cfg.gridsearch = 'yes';
-cfg.grid.unit = 'm';
+cfg.latency         = 0.100;
+cfg.numdipoles      = 2;
+cfg.symmetry        = 'x';
+cfg.gridsearch      = 'yes';
+cfg.grid.unit       = 'm';
 cfg.grid.resolution = 0.02;
-cfg.headmodel = headmodel_meg;
-cfg.channel = 'MEGMAG';
-cfg.senstype = 'MEG';
+
+cfg.senstype        = 'MEG';
+cfg.headmodel       = headmodel_meg;
+cfg.channel         = 'MEGMAG';
 dipole_mag = ft_dipolefitting(cfg, timelock_all);
 
-cfg.headmodel = headmodel_meg;
-cfg.channel = 'MEGGRAD';
+cfg.headmodel       = headmodel_meg;
+cfg.channel         = 'MEGGRAD';
 dipole_grad = ft_dipolefitting(cfg, timelock_all);
 
-cfg.headmodel = headmodel_eeg;
-cfg.channel = 'EEG';
-cfg.senstype = 'EEG';
+cfg.senstype        = 'EEG';
+cfg.headmodel       = headmodel_eeg;
+cfg.channel         = 'EEG';
 dipole_eeg = ft_dipolefitting(cfg, timelock_all);
 
 %% weighted dipole fitting
+
 cfg = [];
 cfg.channel = {'MEGMAG'};
-timelock_sel = ft_selectdata(cfg, timelock_cov);
+timelock_sel = ft_selectdata(cfg, timelock_cov); % this selects channels from the covariance
 
-cfg.numdipoles = 2;
-cfg.symmetry = 'x';
-cfg.gridsearch = 'yes';
-cfg.grid.unit = 'm';
+cfg.latency         = 0.100;
+cfg.numdipoles      = 2;
+cfg.symmetry        = 'x';
+cfg.gridsearch      = 'yes';
+cfg.grid.unit       = 'm';
 cfg.grid.resolution = 0.02;
-cfg.headmodel = headmodel_meg;
-cfg.senstype = 'MEG';
-cfg.latency = 0.100;
+
+cfg.senstype        = 'MEG';
+cfg.headmodel       = headmodel_meg;
 
 cfg.dipfit.noisecov = timelock_sel.cov;
 dipole_mag_weighted = ft_dipolefitting(cfg, timelock_sel);
 
 
 %% combined dipole fitting
+
 cfg = [];
 cfg.channel = {'MEGMAG', 'MEGGRAD'};
-timelock_sel = ft_selectdata(cfg, timelock_cov);
+timelock_sel = ft_selectdata(cfg, timelock_cov); % this selects channels from the covariance
 
-cfg.numdipoles = 2;
-cfg.symmetry = 'x';
-cfg.gridsearch = 'yes';
-cfg.grid.unit = 'm';
+cfg.numdipoles      = 2;
+cfg.latency         = 0.100;
+cfg.symmetry        = 'x';
+cfg.gridsearch      = 'yes';
+cfg.grid.unit       = 'm';
 cfg.grid.resolution = 0.02;
-cfg.headmodel = headmodel_meg;
-cfg.senstype = 'MEG';
-cfg.latency = 0.100;
+
+cfg.senstype        = 'MEG';
+cfg.headmodel       = headmodel_meg;
 
 cfg.dipfit.noisecov = timelock_sel.cov;
 dipole_mag_grad = ft_dipolefitting(cfg, timelock_sel);
 
-cfg.channel = {'MEGMAG', 'EEG'};
-timelock_sel = ft_selectdata(cfg, timelock_cov);
-dipole_mag_eeg = ft_dipolefitting(cfg, timelock_sel);
+if false
+  cfg.channel = {'MEGMAG', 'EEG'};
+  timelock_sel = ft_selectdata(cfg, timelock_cov);
+  
+  cfg.senstype        = {'MEG', 'EEG'};
+  cfg.headmodel       = {headmodel_meg, headmodel_eeg};
+  dipole_mag_eeg = ft_dipolefitting(cfg, timelock_sel);
+end
 
 
 %% plot dipoles
+
 dipoles_all = {dipole_mag, dipole_grad, dipole_eeg, dipole_mag_weighted, dipole_mag_grad};
 
 close all
@@ -196,35 +204,31 @@ colours = {'r', 'g', 'b', 'k', 'm', 'c'};
 ft_plot_axes(headmodel_meg);%, 'edgecolor', 'none', 'facealpha', 0.5);
 hold on
 for i = 1:length(dipoles_all)
-    ft_plot_dipole(dipoles_all{i}.dip.pos, ...
-        reshape(dipoles_all{i}.dip.mom, [3 2]), ...
-        'unit', 'm', 'color', colours{i})
+  ft_plot_dipole(dipoles_all{i}.dip.pos, reshape(dipoles_all{i}.dip.mom, [3 2]), 'unit', 'm', 'color', colours{i})
 end
 view([0 1 0 ])
 
-%% plot on mri
+%% plot dipoles on mri
+
 load mri_resliced
 mri_resliced = ft_convert_units(mri_resliced, 'm');
+
 close all
 figure
 hold on
 
-ft_plot_dipole(dipole_mag_weighted.dip.pos(1,:), mean(dipole_mag_weighted.dip.mom(1:3,:),2), 'color', 'r', 'unit', 'm')
-ft_plot_dipole(dipole_mag_weighted.dip.pos(2,:), mean(dipole_mag_weighted.dip.mom(4:6,:),2), 'color', 'r', 'unit', 'm')
-
-% ft_plot_dipole(source_planar.dip.pos(1,:), mean(source_planar.dip.mom(1:3,:),2), 'color', 'g')
-% ft_plot_dipole(source_planar.dip.pos(2,:), mean(source_planar.dip.mom(4:6,:),2), 'color', 'g')
+for i = 1:length(dipoles_all)
+  ft_plot_dipole(dipoles_all{i}.dip.pos, reshape(dipoles_all{i}.dip.mom, [3 2]), 'unit', 'm', 'color', colours{i})
+end
 
 pos = mean(dipole_mag_weighted.dip.pos,1);
-ft_plot_slice(mri_resliced.anatomy, 'transform',  mri_resliced.transform,  'location', pos, ...
-    'orientation', [1 0 0], 'resolution', 10^0)
-ft_plot_slice(mri_resliced.anatomy, transform',  mri_resliced.transform, 'location', pos, ...
-    'orientation', [0 1 0], 'resolution', 10^0)
-ft_plot_slice(mri_resliced.anatomy, 'transform', mri_resliced.transform, 'location', pos, ...
-    'orientation', [0 0 1], 'resolution', 10^-0)
+ft_plot_slice(mri_resliced.anatomy, 'transform', mri_resliced.transform, 'location', pos, 'orientation', [1 0 0]);
+ft_plot_slice(mri_resliced.anatomy, 'transform', mri_resliced.transform, 'location', pos, 'orientation', [0 1 0]);
+ft_plot_slice(mri_resliced.anatomy, 'transform', mri_resliced.transform, 'location', pos, 'orientation', [0 0 1]);
 
 ft_plot_crosshair(pos, 'color', [1 1 1]/2);
 
+axis auto
 axis tight
 axis off
 
